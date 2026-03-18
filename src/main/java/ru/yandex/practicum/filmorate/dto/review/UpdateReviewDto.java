a5ffd988538c559b00f30dba752aadd90a1e9e4b
fadcon <fad.con0909@gmail.com>
2026-03-19 01:38:57 +0400

    Функциональность «Отзывы»

---
+++ src/main/java/ru/yandex/practicum/filmorate/dto/review/UpdateReviewDto.java
@@ -0,0 +1,33 @@
+package ru.yandex.practicum.filmorate.dto.review;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class UpdateReviewDto {
+    @NotNull
+    @Positive
+    private Long reviewId;
+    @NotBlank
+    @Size(min = 1, max = 200)
+    private String content;
+    @NotNull
+    private Boolean isPositive;
+    @NotNull
+    @Positive
+    private Long userId;
+    @NotNull
+    @Positive
+    private Long filmId;
+
+    public boolean hasContent() {
+        return content != null;
+    }
+
+    public boolean hasIsPositive() {
+        return isPositive != null;
+    }
+}
