c76a0b82b9470d2a6962f713a2f8be996845e3a4
User Yana <ruavky4@yandex.ru>
2026-03-24 18:13:24 +0300

    Checkstyle

--- src/main/java/ru/yandex/practicum/filmorate/storage/event/EventStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/event/EventStorage.java
@@ -4,8 +4,9 @@ import ru.yandex.practicum.filmorate.model.Event;
 import java.util.List;
 
 public interface EventStorage {
-    
+
     Event save(Event event);
+
     List<Event> findFeedByUserId(Long userId, Integer count);
 }
 
